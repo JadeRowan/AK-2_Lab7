d43e3aea232eb0f3335ecadc56faff8918da77a8
Mik Tarnavskyi <miktarnavskyi@gmail.com>
2022-12-15 20:27:58 +0200

    Add files via upload

--- hello.c
+++ hello.c
@@ -10,10 +10,9 @@ MODULE_AUTHOR("Tarnavskyi Mikhailo IO-06");
 MODULE_DESCRIPTION("Lab 7");
 MODULE_LICENSE("Dual BSD/GPL");
 
-static uint count = 1;
-
-module_param(count, uint, 0444);
-MODULE_PARM_DESC(count, "Number of prints");
+static uint amount = 1;
+module_param(amount, uint, 0444);
+MODULE_PARM_DESC(amount, "Amount of outputting Hello worlds");
 
 struct my_struct {
 	struct list_head tlist;
@@ -24,39 +23,36 @@ static LIST_HEAD(my_list_head);
 
 static int __init hello_init(void)
 {
-	if (count == 0) {
+	if (amount == 0) {
 		pr_warn("The parameter is 0");
-	} else if (count >= 5 && count <= 10) {
+	} else if (amount >= 5 && amount <= 10) {
 		pr_warn("The parameter is in [5; 10]");
 	}
-
-	BUG_ON((count > 10));
+	BUG_ON((amount > 10));
 	int i;
-	for (i = 0; i <= count; i++) {
+	for (i = 0; i <= amount; i++) {
 		struct my_struct *ms = NULL;
-		if (i != count)
+		if (i != amount)
 		{
 			ms = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
 		}
 		ms->time = ktime_get();
 		list_add_tail(&ms->tlist, &my_list_head);
-		pr_info("Hello, world!");
+		pr_info("Hello, world!\n");
 	}
-	return 0;
+return 0;
 }
-
 static void __exit hello_exit(void)
 {
-	int count = 0;
+	int amount = 0;
 	struct my_struct *t, *tmp;
 	
 	list_for_each_entry_safe(t, tmp, &my_list_head, tlist) {
-		pr_info("i: %d; ktime = %lld\n", count++, ktime_to_ns(t->time));
+		pr_info("i: %d; ktime = %lld\n", amount++, ktime_to_ns(t->time));
 		list_del(&t->tlist);
 		kfree(t);
-	}
-	pr_info("Module unloaded\n");
 }
-
+pr_info("Module unloaded\n");
+}
 module_init(hello_init);
 module_exit(hello_exit);
